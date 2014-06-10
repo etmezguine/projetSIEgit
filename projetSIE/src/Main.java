f9f91c1a9e19027448624852114f76380bcc63d4
Alexandre Etmezguine <etmezguine@eisti.eu>
2014-06-10 15:20:49 +0200

    lartinoob

--- projetSIE/src/Main.java
+++ projetSIE/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Alex sait pas se servir d'un GIT? il sait juste sucer des BITESSSS");
+		System.out.println("larti commit comme une merde");
 		
 	}
 
